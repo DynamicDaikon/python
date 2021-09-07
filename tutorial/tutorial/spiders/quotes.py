0baa2b49530d147447b613c612d0bb5793b0002d
DynamicDaikon <hietasoba@gmail.com>
2021-09-08 00:34:51 +0900

    scrapy first step comp

--- tutorial/tutorial/spiders/quotes.py
+++ tutorial/tutorial/spiders/quotes.py
@@ -1,10 +1,15 @@
 import scrapy
-
-
-class QuotesSpider(scrapy.Spider):
-    name = 'Quotes'
+from tutorial.items import TutorialItem
+class quotesSpider(scrapy.Spider):
+    name = 'quotes'
     allowed_domains = ['quotes.toscrape.com']
     start_urls = ['http://http://quotes.toscrape.com/']
 
     def parse(self, response):
-        pass
+        for quote in response.css('div.quote'):
+            item = TutorialItem()
+            item['author'] = quote.css('small.author::text').extract_first()
+            item['text'] = quote.css('span.text::text').extract_first()
+            item['tags'] = quote.css('div.tags a.tag::text').extract()
+            yield item
+
