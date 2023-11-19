f51a81683ac7351caae75b1ce467458ab8564529
DynamicDaikon <hietasoba@gmail.com>
2023-11-19 16:17:32 +0900

    fix selenium version(3->4)

--- seleniumSample/seleniumSample.py
+++ seleniumSample/seleniumSample.py
@@ -2,20 +2,19 @@
 
 from curses import KEY_ENTER
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+
 import csv
 
-# WebdriverManager
-from webdriver_manager.chrome import ChromeDriverManager
-
 # ドライバー、URL登録
-# TODO いったん自動ダウンロード固定
-driver = webdriver.Chrome(ChromeDriverManager().install())
+# TODO 暫定バージョンチェックしない。一律最新版ダウンロード。
+driver = webdriver.Chrome()
 driver.get("https://www5.tenseidatanet.co.jp/cb2/office.cgi?")
 
 # 入力要素取得
-id = driver.find_element_by_name("_ID")
-password = driver.find_element_by_name("_Password")
-loginBtn = driver.find_element_by_name("_Submit")
+id = driver.find_element(By.NAME, "_ID")
+password = driver.find_element(By.NAME, "_Password")
+loginBtn = driver.find_element(By.NAME, "_Submit")
 
 # 入力実行
 # TODO パスワード外部ファイル参照させたい。
@@ -25,11 +24,11 @@ password.send_keys("tensei035@")
 loginBtn.click()
 
 # 予定表の画面に遷移
-schedule = driver.find_element_by_link_text("スケジュール")
+schedule = driver.find_element(By.LINK_TEXT, "スケジュール")
 schedule.click()
 # csv ファイル読み込み
 csv_file = open(
-    "/Users/takumaueyama/git/projects/python/seleniumSample/schedule.csv",
+    "/Users/dynamic_daikon/git/projects/python/seleniumSample/schedule.csv",
     "r",
     encoding="UTF-8",
     errors="",
@@ -49,62 +48,62 @@ for row in f:
     # rowはList
     # row[0]で必要な項目を取得することができる
     # form要素取得
-    form = driver.find_element_by_xpath(
+    form = driver.find_element(By.XPATH,
         "/html/body/form/table[2]/tbody/tr[2]/td[2]/a[2]"
     )
 
     form.click()
 
-    setStrtMonth = driver.find_element_by_xpath(
+    setStrtMonth = driver.find_element(By.XPATH,
         "/html/body/form/table/tbody/tr[1]/td[2]/select[1]"
     )
     setStrtMonth.send_keys(row[0])
 
-    setStrtDay = driver.find_element_by_xpath(
+    setStrtDay = driver.find_element(By.XPATH,
         "/html/body/form/table/tbody/tr[1]/td[2]/select[2]"
     )
     setStrtDay.send_keys(row[1])
 
-    setStrtHour = driver.find_element_by_xpath(
+    setStrtHour = driver.find_element(By.XPATH,
         "/html/body/form/table/tbody/tr[1]/td[2]/select[3]"
     )
     setStrtHour.send_keys(row[2])
 
-    setStrtMinute = driver.find_element_by_xpath(
+    setStrtMinute = driver.find_element(By.XPATH,
         "/html/body/form/table/tbody/tr[1]/td[2]/select[4]"
     )
     setStrtMinute.send_keys(row[3])
 
-    setEndMonth = driver.find_element_by_xpath(
+    setEndMonth = driver.find_element(By.XPATH,
         "/html/body/form/table/tbody/tr[1]/td[2]/select[5]"
     )
     setEndMonth.send_keys(row[0])
 
-    setEndDay = driver.find_element_by_xpath(
+    setEndDay = driver.find_element(By.XPATH,
         "/html/body/form/table/tbody/tr[1]/td[2]/select[6]"
     )
     setEndDay.send_keys(row[1])
 
-    setEndHour = driver.find_element_by_xpath(
+    setEndHour = driver.find_element(By.XPATH,
         "/html/body/form/table/tbody/tr[1]/td[2]/select[7]"
     )
     setEndHour.send_keys(row[4])
 
-    setEndMinute = driver.find_element_by_xpath(
+    setEndMinute = driver.find_element(By.XPATH,
         "/html/body/form/table/tbody/tr[1]/td[2]/select[8]"
     )
     setEndMinute.send_keys(row[5])
 
-    setStatus = driver.find_element_by_xpath(
+    setStatus = driver.find_element(By.XPATH,
         "/html/body/form/table/tbody/tr[2]/td[2]/select"
     )
     setStatus.send_keys("【□予定】")
 
-    setTask = driver.find_element_by_xpath(
+    setTask = driver.find_element(By.XPATH,
         "/html/body/form/table/tbody/tr[2]/td[2]/input"
     )
     setTask.send_keys(row[6])
     # setTask.send_keys(KEY_ENTER)
 
-    submit = driver.find_element_by_xpath("/html/body/form/input[7]")
+    submit = driver.find_element(By.XPATH, "/html/body/form/input[7]")
     submit.click()
